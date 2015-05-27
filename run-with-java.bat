c17c552c1fdc4275b0d273a655db3d68c12d6cce
Hakan Saglam <hakan.saglam@gmail.com>
2015-05-27 16:34:47 +0300

    Update run-with-java.bat

--- run-with-java.bat
+++ run-with-java.bat
@@ -1,23 +1,27 @@
-#!/bin/sh
-pisti_classpath="$(pwd)/classes"
-#echo $pisti_classpath
-rm -rf $pisti_classpath
-mkdir $pisti_classpath
+set home=%cd% 
+set pisti_classpath="%cd%\classes"
 
-cd src/main/java
-javac -d $pisti_classpath \
-	net/peakgames/pisti/*.java \
-	net/peakgames/pisti/bot/*.java \
-	net/peakgames/pisti/deck/*.java \
-	net/peakgames/pisti/game/*.java \
+echo %pisti_classpath%
+
+rmdir %pisti_classpath%
+mkdir %pisti_classpath%
+
+cd src\main\java
+
+javac -d %pisti_classpath%^
+	net/peakgames/pisti/*.java^
+	net/peakgames/pisti/bot/*.java^
+	net/peakgames/pisti/deck/*.java^
+	net/peakgames/pisti/game/*.java^
 	net/peakgames/pisti/utility/*.java
-	   
-java -cp $pisti_classpath \
-	net/peakgames/pisti/PistiBootstrap \
-    1 \
-    10 \
-    net.peakgames.pisti.bot.DummyBot \
-    net.peakgames.pisti.bot.DummyBot \
-    net.peakgames.pisti.bot.RandomBot \
-    net.peakgames.pisti.bot.RandomBot \
-    
+
+java -cp %pisti_classpath%^
+	net/peakgames/pisti/PistiBootstrap^
+	1^
+	10^
+	net.peakgames.pisti.bot.DummyBot^
+	net.peakgames.pisti.bot.DummyBot^
+	net.peakgames.pisti.bot.RandomBot^
+	net.peakgames.pisti.bot.RandomBot
+ 
+cd %home%
